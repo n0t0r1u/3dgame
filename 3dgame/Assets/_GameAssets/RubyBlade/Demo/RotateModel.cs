a0a65fa95415dc66d2e26366cfbb2584f3f750b1
n0t0r1u <erhandh4@gmail.com>
2025-06-14 17:15:24 +0300

    v0.8.1 Character Changes

---
+++ 3dgame/Assets/_GameAssets/RubyBlade/Demo/RotateModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour
+{
+    public float speed = 1;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(0, 1 * speed, 0);
+    }
+}
